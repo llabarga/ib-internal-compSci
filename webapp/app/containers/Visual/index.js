2be5f64eaed1f2ddf45c86bd2a7742edb6b4e070
Leyre Labarga <LLabarga@MacBook-Pro-de-Leyre.local>
2020-09-27 10:03:28 +0200

    Add exams

--- webapp/app/containers/Visual/index.js
+++ webapp/app/containers/Visual/index.js
@@ -14,6 +14,7 @@ import {
   EuiCard,
   EuiIcon,
   EuiFlexGroup,
+  EuiFlexGrid,
   EuiFlexItem,
   EuiButton,
 } from '@elastic/eui';
@@ -42,44 +43,227 @@ function Visual() {
 
   const timers =[
     {
-      id:1,
-      name: 'Prueba 1',
-      subject: 'Castellano',
-      level: 'NM',
-      length: 120,
-      readingTime: false,
-      icon:'Cloud',
-    },
+    id:1,
+    name: 'Prueba 1',
+    subject: 'Lengua B',
+    level: 'NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
 
-    {
-      id:2,
-      name: 'Prueba 2',
-      subject: 'Biología',
-      level: 'NS',
-      length: 60,
-      readingTime: false,
-      icon:'Cloud',
-    },
+  {
+    id:2,
+    name: 'Prueba 2',
+    subject: 'Lengua B',
+    level: 'NS',
+    length: 60,
+    readingTime: true,
+    icon:'Kibana',
+  },
 
-    {
-      id:3,
-      name: 'Prueba 2',
-      subject: 'English',
-      level: 'NS',
-      length: 120,
-      readingTime: true,
-      icon:'Kibana',
-    }
+  {
+    id:3,
+    name: 'Prueba 1',
+    subject: 'Biología',
+    level: 'NS/NM',
+    length: 45,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:4,
+    name: 'Prueba 2',
+    subject: 'Biología',
+    level: 'NS/NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:5,
+    name: 'Prueba 1',
+    subject: 'Física',
+    level: 'NS/NM',
+    length: 45,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:6,
+    name: 'Prueba 2',
+    subject: 'Física',
+    level: 'NS/NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:7,
+    name: 'Prueba 1',
+    subject: 'Filosofía',
+    level: 'NS/NM',
+    length: 60,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:8,
+    name: 'Prueba 2',
+    subject: 'Filosofía',
+    level: 'NS/NM',
+    length: 60,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:9,
+    name: 'Prueba 3',
+    subject: 'Filosofía',
+    level: 'NS/NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:10,
+    name: 'Prueba 1',
+    subject: 'Historia',
+    level: 'NS/NM',
+    length: 60,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:11,
+    name: 'Prueba 2',
+    subject: 'Historia',
+    level: 'NS/NM',
+    length: 45,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:12,
+    name: 'Prueba 1',
+    subject: 'Matemáticas',
+    level: 'NS/NM',
+    length: 90,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:13,
+    name: 'Prueba 2',
+    subject: 'Matemáticas',
+    level: 'NS/NM',
+    length: 90,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:14,
+    name: 'Prueba 3',
+    subject: 'Matemáticas',
+    level: 'NS',
+    length: 60,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+
+  {
+    id:15,
+    name: 'Prueba 1',
+    subject: 'Computer Science',
+    level: 'NM',
+    length: 90,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:16,
+    name: 'Prueba 3',
+    subject: 'Computer Science',
+    level: 'NS',
+    length: 60,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:17,
+    name: 'Prueba 1',
+    subject: 'Gestión Empresarial',
+    level: 'NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:18,
+    name: 'Prueba 2',
+    subject: 'Gestión Empresarial',
+    level: 'NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:19,
+    name: 'Prueba 1',
+    subject: 'Química',
+    level: 'NS/NM',
+    length: 45,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:20,
+    name: 'Prueba 2',
+    subject: 'Química',
+    level: 'NS/NM',
+    length: 75,
+    readingTime: false,
+    icon:'Cloud',
+  },
+
+  {
+    id:21,
+    name: 'Prueba 1',
+    subject: 'Lengua A',
+    level: 'NM',
+    length: 90,
+    readingTime: false,
+    icon:'Cloud',
+  },
   ];
+
+  const ITEM_STYLE = { width: '300px' };
 
   const cardNodes = timers.map(function(item, index) {
   return (
-    <EuiFlexItem key={index}>
+    <EuiFlexItem key={index} style={ITEM_STYLE}>
     <EuiCard
      icon={<EuiIcon size="xxl" type={`logo${item.icon}`} />}
-     title={item.name}
+     title={item.subject}
      isDisabled={item.icon === 'Kibana' ? true : false}
-     description={item.subject + "  "+item.length + " minutos " + item.level}
+     description={item.name + item.level+ item.length + " minutos " }
      onClick={() => window.alert('Timeout')}
     />
 
@@ -93,8 +277,8 @@ function Visual() {
 
        These are your timers!
 
-       <EuiFlexGroup gutterSize="l">{cardNodes}</EuiFlexGroup>
-       <EuiButton> Add </EuiButton>
+       <EuiFlexGrid gutterSize="l" columns={2}>{cardNodes}</EuiFlexGrid>
+
        </>
 
 
